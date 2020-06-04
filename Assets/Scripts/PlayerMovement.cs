d5385086f7e6c6e2eabe4a2dcf0159af90c08a75
Lila <kasamatsulila@gmail.com>
2020-06-04 14:54:31 +0200

    added simple slingshot movement
    basic organs
    moving player character
    slingshot line

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float power = 10f;
+
+    [SerializeField] Vector2 minPower;
+    [SerializeField] Vector2 maxPower;
+
+    private Rigidbody2D rb;
+    private Camera camera;
+    private SlingshotLine slingshotLine;
+    private Vector2 force;
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+        slingshotLine = GetComponent<SlingshotLine>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 0;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 0;
+
+            slingshotLine.RenderLine(startPoint, currentPoint);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 0;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x),
+                                Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+            slingshotLine.EndLine();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/SlingshotLine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class SlingshotLine : MonoBehaviour
+{
+    [SerializeField] LineRenderer lr;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    public void RenderLine(Vector3 startPoint, Vector3 endPoint)
+    {
+        lr.positionCount = 2;
+        Vector3[] vertices = new Vector3[2];
+        vertices[0] = startPoint;
+        vertices[1] = endPoint;
+
+        lr.SetPositions(vertices);
+    }
+
+    public void EndLine()
+    {
+        lr.positionCount = 0;
+    }
+}
